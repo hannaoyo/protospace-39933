5b4a0c88b391d5a53515c97235af649ee3ab22a4
hannaoyo <hannaoyo11@gmail.com>
2023-12-26 12:59:25 +0900

    修正３

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :move_to_index, except: [:show, :new, :create]
+  before_action :authenticate_user!, except: [ :show]
   def show
     @user = User.find(params[:id])
     @prototypes = @user.prototypes
